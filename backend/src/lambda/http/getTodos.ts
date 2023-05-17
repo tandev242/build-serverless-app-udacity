3d13eda391a8922a370a08d28aa5110971f659d8
Tan Nguyen <superjunior242@gmail.com>
2023-05-18 00:12:04 +0700

    update code

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,17 +4,20 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getTodosForUser } from '../../helpers/todos'
 import { getUserId } from '../utils';
 
-// TODO: Get all TODO items for a current user
+// DONE: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
-
-    return undefined
-
+    const userId = getUserId(event)
+    const items = await getTodosForUser(userId)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({items})
+    }
+  }
+)
 handler.use(
   cors({
     credentials: true
--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../businessLogic/todos'
+import { updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
@@ -12,10 +12,14 @@ export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
-
-    return undefined
+    const userId = getUserId(event)
+    // DONE: Update a TODO item with the provided id using values in the "updatedTodo" object
+    await updateTodo(todoId, userId, updatedTodo)
+    return {
+      statusCode: 202,
+      body: JSON.stringify({})
+    }
+  }
 )
 
 handler
